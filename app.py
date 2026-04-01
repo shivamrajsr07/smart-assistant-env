9e874423900f33e65ac949b81fdf1c202377a729
Shivam Raj <shivamraj.connect@gmail.com>
2026-04-02 00:29:35 +0530

    fix: move app.py to root for HF

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from openenv_core.env_server.http_server import create_app
 from server.smart_assistant_env_environment import SmartAssistantEnvironment
+from models import AssistantAction, AssistantObservation
 
 env = SmartAssistantEnvironment()
-app = create_app(env)
+
+app = create_app(
+    env,
+    AssistantAction,
+    AssistantObservation,
+)
